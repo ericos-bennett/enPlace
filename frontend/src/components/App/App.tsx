0e4e6d95172faa35782d4648fd66ca3da45e7212
Eric Bennett <ericos.bennett@gmail.com>
2024-06-06 12:42:39 -0700

    show multiple recipes on page

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -4,22 +4,28 @@ import { Recipe } from '../Recipe/Recipe'
 import type { Recipe as RecipeType } from '../../types'
 
 const App: React.FC = () => {
-  const testRecipePath: string = '/src/assets/testRecipe3.json'
+  const testRecipePath: string = '/src/assets/testRecipes.json'
 
-  const [recipe, setRecipe] = useState<RecipeType | null>(null);
+  const [recipes, setRecipes] = useState<RecipeType[] | null>(null);
 
   useEffect(() => {
     fetch(testRecipePath)
       .then(response => response.json())
-      .then(data => setRecipe(data))
+      .then(data => setRecipes(data))
       .catch(error => console.error('Error loading JSON:', error))
   }, []);
 
   return (
     <>
-      {!recipe ? (
+      {!recipes ? (
         <div>Loading...</div>
-      ) : <Recipe recipe={recipe}></Recipe>
+      ) : (
+        <div>
+          {recipes.map((recipe, recipeIndex) => (
+            <Recipe recipe={recipe} key={recipeIndex}></Recipe>
+          ))}
+        </div>
+      )
       }</>
   )
 }
