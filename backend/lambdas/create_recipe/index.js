d21302bdf72beffdc8a788d5e82abdcf1496deaa
Eric Bennett <ericos.bennett@gmail.com>
2024-06-11 22:48:01 -0700

    refactor createRecipe lambda

--- backend/lambdas/create_recipe/index.js
+++ backend/lambdas/create_recipe/index.js
@@ -1,76 +1,46 @@
-const testJson = {
-  'UserId': 123,
-  'Timestamp': 1623064800,
-  "name": "Sausage Pasta",
-  "description": "A family favorite full of healthy ingredients",
-  "author": "Eric Bennett",
-  "servings": 4,
-  "prepTime": 15,
-  "cookingTime": 45,
-  "steps": [
-    {
-      "ingredients": [
-        {
-          "ingredient": "Olive Oil",
-          "preparation": null,
-          "amount": 2,
-          "units": "Tbsp"
-        }
-      ],
-      "instructions": "Heat oil in a large pan"
-    },
-    {
-      "ingredients": [
-        {
-          "ingredient": "Tomatoes",
-          "preparation": "Diced",
-          "amount": 4,
-          "units": null
-        },
-        {
-          "ingredient": "Basil",
-          "preparation": "Sliced",
-          "amount": 10,
-          "units": "Leaves"
-        }
-      ],
-      "instructions": "Combine tomatoes and basil"
-    }
-  ]
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+
+// Update this to call OpenAI API
+const getRecipeJson = (recipeUrl) => {
+  const filePath = path.join(__dirname, 'testRecipe.json');
+  return JSON.parse(fs.readFileSync(filePath, 'utf8'));
 }
 
-
-const AWS = require('aws-sdk');
-
-const dynamodb = new AWS.DynamoDB.DocumentClient({
-  endpoint: 'http://localstack:4566',
-  region: 'us-east-1'
-});
-
-
 exports.handler = async (event) => {
+  const { recipeUrl } = JSON.parse(event.body);
+  recipeItem = getRecipeJson(recipeUrl)
+
+  const dynamodb = new AWS.DynamoDB.DocumentClient({
+    endpoint: 'http://localstack:4566',
+    region: 'us-east-1'
+  });
+
+  headers = {
+    'Access-Control-Allow-Origin': '*'
+  }
+
   const params = {
     TableName: 'Recipes',
-    Item: testJson
+    Item: recipeItem
   };
 
   try {
     await dynamodb.put(params).promise();
+    console.log(`Item from URL {${recipeUrl}} added to Recipes table!`)
     return {
       statusCode: 201,
-      body: JSON.stringify('Item added to Recipes table!'),
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
+      body: recipeItem,
+      headers
     };
+
   } catch (error) {
     console.log(error)
     return {
       statusCode: 500,
       body: JSON.stringify(error),
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
+      headers
     };
   }
 };
