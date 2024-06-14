c7f4ccf0220aaca2ee95d5f10e826d8bfbb3fb23
Eric Bennett <ericos.bennett@gmail.com>
2024-06-14 14:44:34 -0700

    add frontend environment variables

--- frontend/src/services/recipe.ts
+++ frontend/src/services/recipe.ts
@@ -1,14 +1,14 @@
 import { Recipe } from '../types'
 
-const localApiGatewayId = '6j1oyjvcio'
+const recipesEndpoint = `${import.meta.env.VITE_API_URL}:${import.meta.env.VITE_API_PORT}/${import.meta.env.VITE_API_STAGE}/recipes`
 
 export const getRecipes = async (): Promise<Recipe[]> => {
-  const response = await fetch(`https://${localApiGatewayId}.execute-api.localhost.localstack.cloud:4566/local/recipes`);
+  const response = await fetch(recipesEndpoint);
   return response.json()
 }
 
 export const createRecipe = async (recipeUrl: string): Promise<Recipe> => {
-  const response = await fetch(`https://${localApiGatewayId}.execute-api.localhost.localstack.cloud:4566/local/recipes`, {
+  const response = await fetch(recipesEndpoint, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
--- infra/main.tf
+++ infra/main.tf
@@ -314,3 +314,7 @@ resource "aws_dynamodb_table" "recipes" {
     type = "N"
   }
 }
+
+output "api_gateway_id" {
+  value = aws_api_gateway_rest_api.menu_api.id
+}
