ee2bbc74200ee773126825fe441baafa6108995f
Eric Bennett <ericos.bennett@gmail.com>
2024-06-24 13:45:21 -0700

    center 404 page

--- frontend/src/pages/NotFoundPage/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,11 @@
+import { Container } from '@mui/material'
 import './NotFoundPage.css'
 
 export const NotFoundPage: React.FC = () => {
   return (
-    <div className="not-found-page">
+    <Container className="not-found-container">
       <h1>404 - Not Found</h1>
       <p>The page you are looking for does not exist.</p>
-    </div>
+    </Container>
   )
 }
