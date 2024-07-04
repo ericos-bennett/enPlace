b6e5117b8301c635cb4d64ec00c2f27e83b2d485
Eric Bennett <ericos.bennett@gmail.com>
2024-07-04 15:24:05 -0700

    use localhost temporarily as route53 records

--- infra/prod/route53.tf
+++ infra/prod/route53.tf
@@ -7,6 +7,7 @@ resource "aws_route53_record" "enplace_fe" {
   name    = var.domain_name
   type    = "A"
 
+  records = ["127.0.0.1"]
   # alias {
   #   name                   = aws_cloudfront_distribution.enplace_fe.domain_name
   #   zone_id                = aws_cloudfront_distribution.enplace_fe.hosted_zone_id
@@ -19,6 +20,7 @@ resource "aws_route53_record" "enplace_fe_www" {
   name    = "www.${var.domain_name}"
   type    = "A"
 
+  records = ["127.0.0.1"]
   # alias {
   #   name                   = aws_cloudfront_distribution.enplace_fe.domain_name
   #   zone_id                = aws_cloudfront_distribution.enplace_fe.hosted_zone_id
@@ -36,6 +38,7 @@ resource "aws_route53_record" "enplace_api" {
   name    = "api.${var.domain_name}"
   type    = "A"
 
+  records = ["127.0.0.1"]
   # alias {
   #   name                   = aws_api_gateway_domain_name.enplace.cloudfront_domain_name
   #   zone_id                = aws_api_gateway_domain_name.enplace.cloudfront_zone_id
@@ -54,9 +57,10 @@ resource "aws_route53_record" "enplace_auth" {
   name    = "auth.${var.domain_name}"
   type    = "A"
 
-  alias {
-    name                   = aws_cognito_user_pool_domain.enplace.cloudfront_distribution
-    zone_id                = aws_cognito_user_pool_domain.enplace.cloudfront_distribution_zone_id
-    evaluate_target_health = false
-  }
+  records = ["127.0.0.1"]
+  # alias {
+  #   name                   = aws_cognito_user_pool_domain.enplace.cloudfront_distribution
+  #   zone_id                = aws_cognito_user_pool_domain.enplace.cloudfront_distribution_zone_id
+  #   evaluate_target_health = false
+  # }
 }
