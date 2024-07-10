8e215aa38abf192809d56b4430f1cc8629e4dc9d
Eric Bennett <ericos.bennett@gmail.com>
2024-07-09 23:34:35 -0700

    add OPTIONS preflight for recipe subresource

--- infra/prod/apigateway.tf
+++ infra/prod/apigateway.tf
@@ -51,12 +51,6 @@ resource "aws_api_gateway_resource" "recipes" {
   path_part   = "recipes"
 }
 
-resource "aws_api_gateway_resource" "recipe" {
-  rest_api_id = aws_api_gateway_rest_api.enplace.id
-  parent_id   = aws_api_gateway_resource.recipes.id
-  path_part   = "{recipeId}"
-}
-
 resource "aws_api_gateway_method" "options_recipes" {
   rest_api_id   = aws_api_gateway_rest_api.enplace.id
   resource_id   = aws_api_gateway_resource.recipes.id
@@ -100,6 +94,65 @@ resource "aws_api_gateway_integration_response" "options_recipes" {
   rest_api_id = aws_api_gateway_rest_api.enplace.id
   resource_id = aws_api_gateway_resource.recipes.id
   http_method = aws_api_gateway_method.options_recipes.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"      = "'*'",
+    "method.response.header.Access-Control-Allow-Methods"     = "'GET,POST,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
+  }
+}
+
+resource "aws_api_gateway_resource" "recipe" {
+  rest_api_id = aws_api_gateway_rest_api.enplace.id
+  parent_id   = aws_api_gateway_resource.recipes.id
+  path_part   = "{recipeId}"
+}
+
+resource "aws_api_gateway_method" "options_recipe" {
+  rest_api_id   = aws_api_gateway_rest_api.enplace.id
+  resource_id   = aws_api_gateway_resource.recipe.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options_recipe" {
+  rest_api_id = aws_api_gateway_rest_api.enplace.id
+  resource_id = aws_api_gateway_resource.recipe.id
+  http_method = aws_api_gateway_method.options_recipe.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"      = true,
+    "method.response.header.Access-Control-Allow-Methods"     = true,
+    "method.response.header.Access-Control-Allow-Headers"     = true,
+    "method.response.header.Access-Control-Allow-Credentials" = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration" "options_recipe" {
+  rest_api_id = aws_api_gateway_rest_api.enplace.id
+  resource_id = aws_api_gateway_resource.recipe.id
+  http_method = aws_api_gateway_method.options_recipe.http_method
+  type        = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_recipe" {
+  depends_on = [
+    aws_api_gateway_integration.options_recipe
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.enplace.id
+  resource_id = aws_api_gateway_resource.recipe.id
+  http_method = aws_api_gateway_method.options_recipe.http_method
   status_code = "200"
 
   response_parameters = {
