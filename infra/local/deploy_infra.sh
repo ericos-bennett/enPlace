f75a58de004625a78b95c2e580ac1d7c84fdc81b
Eric Bennett <ericos.bennett@gmail.com>
2024-07-22 22:15:38 -0400

    combine package_lambdas code for local and prod

--- infra/local/deploy_infra.sh
+++ infra/local/deploy_infra.sh
@@ -2,62 +2,9 @@
 set -e
 
 # Package lambda code
-echo ">>> Updating get_recipe lambda"
-cd ../../backend/get_recipe
-pip install \
-  --upgrade \
-  --quiet \
-  --python-version 3.12 \
-  --platform manylinux2014_aarch64 \
-  --implementation cp \
-  --only-binary=:all: \
-  --no-deps \
-  --target package \
-  -r requirements.txt
-cd package && zip -q -r ../get_recipe.zip .
-cd .. && zip get_recipe.zip main.py
-mv get_recipe.zip ../../infra/local
-
-echo ">>> Updating get_recipes lambda"
-cd ../get_recipes
-pip install \
-  --upgrade \
-  --quiet \
-  --python-version 3.12 \
-  --platform manylinux2014_aarch64 \
-  --implementation cp \
-  --only-binary=:all: \
-  --no-deps \
-  --target package \
-  -r requirements.txt
-cd package && zip -q -r ../get_recipes.zip .
-cd .. && zip get_recipes.zip main.py
-mv get_recipes.zip ../../infra/local
-
-echo ">>> Updating create_recipe lambda"
-cd ../create_recipe
-pip install \
-  --upgrade \
-  --quiet \
-  --python-version 3.12 \
-  --platform manylinux2014_aarch64 \
-  --implementation cp \
-  --only-binary=:all: \
-  --no-deps \
-  --target package \
-  -r requirements_binary.txt
-# We have to install these manually, because there are no wheels with the correct version on PyPI
-pip install \
-  --upgrade \
-  --quiet \
-  --target package \
-  -r requirements_source.txt
-cd package && zip -q -r ../create_recipe.zip .
-cd .. && zip create_recipe.zip main.py && zip create_recipe.zip exampleRecipe.json
-mv create_recipe.zip ../../infra/local
-
-cd ../../infra/local
-echo ">>> Lambda code updated"
+cd ..
+./package_lambdas.sh local
+cd local
 
 # Copy terraform files that we want to use for the local deployment
 echo ">>> Copying terraform resource files to local directory"
--- infra/package_lambdas.sh
+++ infra/package_lambdas.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -e
 
+# Check if env variable is passed
+if [ -z "$1" ]; then
+  echo "Environment variable not provided. Usage: $0 <env>"
+  exit 1
+fi
+ENV=$1
+
 # Package lambda code
+echo ">>> Packaging lambdas"
+cd ../backend
+
 echo ">>> Updating get_recipe lambda"
-cd ../../backend/get_recipe
+cd get_recipe
 pip install \
   --upgrade \
   --quiet \
@@ -16,10 +26,11 @@ pip install \
   -r requirements.txt
 cd package && zip -q -r ../get_recipe.zip .
 cd .. && zip get_recipe.zip main.py
-mv get_recipe.zip ../../infra/prod
+mv get_recipe.zip ../../infra/$ENV
+cd ..
 
 echo ">>> Updating get_recipes lambda"
-cd ../get_recipes
+cd get_recipes
 pip install \
   --upgrade \
   --quiet \
@@ -32,10 +43,11 @@ pip install \
   -r requirements.txt
 cd package && zip -q -r ../get_recipes.zip .
 cd .. && zip get_recipes.zip main.py
-mv get_recipes.zip ../../infra/prod
+mv get_recipes.zip ../../infra/$ENV
+cd ..
 
 echo ">>> Updating create_recipe lambda"
-cd ../create_recipe
+cd create_recipe
 pip install \
   --upgrade \
   --quiet \
@@ -54,6 +66,6 @@ pip install \
   -r requirements_source.txt
 cd package && zip -q -r ../create_recipe.zip .
 cd .. && zip create_recipe.zip main.py && zip create_recipe.zip exampleRecipe.json
-mv create_recipe.zip ../../infra/prod
+mv create_recipe.zip ../../infra/$ENV
 
 echo ">>> Lambda code updated"
