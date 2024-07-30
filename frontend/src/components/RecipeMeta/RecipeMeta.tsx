6c9889824ceddd6d54e7d24df991a3b20c73617e
Eric Bennett <ericos.bennett@gmail.com>
2024-07-30 12:12:37 -0700

    remove recipe from FE state on delete

--- frontend/src/components/RecipeMeta/RecipeMeta.tsx
+++ frontend/src/components/RecipeMeta/RecipeMeta.tsx
@@ -2,22 +2,25 @@ import { ListItemButton, ListItemText, IconButton } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import type { RecipeMeta as RecipeMetaType } from '~/types'
 import garbage from '~/assets/icons/garbage.svg'
-import { deleteRecipe } from '~/services/recipe'
 
 interface RecipeMetaProps {
   recipeMeta: RecipeMetaType
+  onDelete: (recipeId: string) => Promise<void>
 }
 
-export const RecipeMeta: React.FC<RecipeMetaProps> = ({ recipeMeta }) => {
+export const RecipeMeta: React.FC<RecipeMetaProps> = ({
+  recipeMeta,
+  onDelete,
+}) => {
   const navigate = useNavigate()
 
   const handleClick = (): void => {
     navigate(`/recipes/${recipeMeta.Id}`)
   }
 
-  const handleDelete = async (event: React.MouseEvent): Promise<void> => {
+  const handleDelete = (event: React.MouseEvent): void => {
     event.stopPropagation()
-    const response = await deleteRecipe(recipeMeta.Id)
+    onDelete(recipeMeta.Id)
   }
 
   return (
