b3badc961aba07eddd428f08f0854815127f5ab2
Eric Bennett <ericos.bennett@gmail.com>
2024-08-09 11:47:16 -0700

    remove image from homepage

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,6 @@
 import { Container, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import { CreateRecipeForm } from '~/components/CreateRecipeForm/CreateRecipeForm'
-import homeImage from '~/assets/images/home-image.jpeg'
 import './HomePage.css'
 
 export const HomePage: React.FC = () => {
@@ -17,12 +16,6 @@ export const HomePage: React.FC = () => {
         Reliable recipes, no scrolling
       </Typography>
       <CreateRecipeForm onCreateRecipe={handleCreateRecipe} />
-      <img
-        src={homeImage}
-        className="home-image"
-        width="400px"
-        alt="Home Image"
-      />
     </Container>
   )
 }
