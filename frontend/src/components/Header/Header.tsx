a4614a245b04f48a342446e965249a869e714f0a
Eric Bennett <ericos.bennett@gmail.com>
2024-08-09 12:24:01 -0700

    add logo to header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom'
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { LoginButton } from '../LoginButton/LoginButton'
 import { useAuthStore } from '~/store/auth'
+import logo from '~/assets/logo.png'
 import './Header.css'
 
 export const Header: React.FC = () => {
@@ -9,9 +10,10 @@ export const Header: React.FC = () => {
 
   return (
     <header>
-      <Typography variant="body1" className="nav-link">
-        <Link to="/">enPlace</Link>
-      </Typography>
+      <Link to="/" className="nav-link">
+        <Box component="img" src={logo}></Box>
+        <Typography variant="body1">enPlace</Typography>
+      </Link>
       <nav>
         {isLoggedIn && (
           <Typography variant="body1" className="nav-link">
